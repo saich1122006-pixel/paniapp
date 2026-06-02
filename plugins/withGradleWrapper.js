9f661a1e3b9b752e5e1e5aab08bf8ce21b7e2b8e
saich1122006-pixel.com <saich1122006@gmail.com>
2026-06-02 11:46:58 +0530

    Fix Android build by downgrading Gradle to 8.10.2

---
+++ plugins/withGradleWrapper.js
@@ -0,0 +1,26 @@
+const { withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+const withGradleWrapper = (config, version) => {
+  return withDangerousMod(config, [
+    'android',
+    async (config) => {
+      const wrapperPath = path.join(
+        config.modRequest.platformProjectRoot,
+        'gradle/wrapper/gradle-wrapper.properties'
+      );
+      if (fs.existsSync(wrapperPath)) {
+        let contents = fs.readFileSync(wrapperPath, 'utf-8');
+        contents = contents.replace(
+          /distributionUrl=.*/g,
+          `distributionUrl=https\\://services.gradle.org/distributions/gradle-${version}-bin.zip`
+        );
+        fs.writeFileSync(wrapperPath, contents);
+      }
+      return config;
+    },
+  ]);
+};
+
+module.exports = (config, props) => withGradleWrapper(config, props?.version || '8.10.2');
